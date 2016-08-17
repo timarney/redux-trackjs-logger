e23033c0a1302c6e5f944a54a48522bb7aade752
Tim Arney <tim@line37.com>
2016-08-17 01:55:36 -0400

    added download route

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/*
+DEMO SERVER FOR SAVING FILES
+*/
+
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
@@ -7,7 +11,8 @@ var JsonDB = require('node-json-db')
 var express = require('express')
 var app = express()
 var port = 3000
-var path = require('path');
+var path = require('path')
+fs = require('fs')
 
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
@@ -22,7 +27,14 @@ app.use(bodyParser.urlencoded({
 
 app.use('/logs', express.static(path.join(__dirname + '/logs')));
 
-// for demo purposes don't do this
+app.get('/download/*', function (req, res) {
+  var data = JSON.parse(fs.readFileSync(path.join(__dirname + '/logs/')+req.params[0]), 'utf8')
+  res.attachment(req.params[0])
+  res.setHeader('Content-Type', 'application/octet-stream')
+  res.end(JSON.stringify(data, null, 2), 'utf8')
+})
+
+
 app.post('/savejson', function (req, res) {
   var filename = req.body.filename
   var db = new JsonDB('logs/' + filename, true, false)
