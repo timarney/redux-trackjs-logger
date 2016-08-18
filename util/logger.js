134b4eba8645efb78ec0f14ae73094b4df53c9c0
Tim Arney <tim@line37.com>
2016-08-18 11:30:40 -0400

    testing remote dev tools integration

--- util/logger.js
+++ util/logger.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-let jsonURL = 'https://api.myjson.com/bins'
+let jsonURL = 'http://localhost:8000'
+let reportURL = `${jsonURL}?remotedev_report=`
 let token = window._trackJs.token
 let trackJsCaptureUrl = `https://capture.trackjs.com/capture?token=${token}`
 let actions = []
@@ -8,8 +9,10 @@ let supressNextScriptError = false
 function trackError (payload) {
   supressNextScriptError = true
   // save actions to a json file (myjson.com)
-  axios.post(jsonURL, actions)
+  const postData = {type: 'ACTIONS',payload: actions }
+  axios.post(jsonURL, postData)
     .then(function (response) {
+      // console.log(response)
       sendError(response, payload)
     })
     .catch(function (err) {
@@ -18,7 +21,7 @@ function trackError (payload) {
 }
 
 function sendError (response, payload) {
-  let filename = response.data.uri
+  let filename = `${reportURL}${response.data.id}`
   // create a reference to the json file and send to TrackJS
   let data = {message: filename, severity: 'log', timestamp: payload.timestamp}
   payload.console.push(data)
@@ -33,7 +36,7 @@ export function configureTracker () {
 
   window.trackJs.configure({
     onError: (payload, error) => {
-      if(payload.message.indexOf('Script error') >= 0 && supressNextScriptError){
+      if (payload.message.indexOf('Script error') >= 0 && supressNextScriptError) {
         supressNextScriptError = false
         return false
       }
